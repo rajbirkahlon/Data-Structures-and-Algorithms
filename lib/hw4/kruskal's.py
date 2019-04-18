a4c099a52d66abed5d6cd02389efd380ebe09f9f
Raj <RajbirSingh1996@gmail.com>
2019-04-17 23:08:55 -0700

    kruskal written, not able to successfully able to import union find module from hw1's union_find.py file

--- lib/hw4/kruskal's.py
+++ lib/hw4/kruskal's.py
@@ -1,18 +1,30 @@
-from collections import defaultdict 
-  
+from collections import defaultdict
+import PriorityQueue
+from hw1 import union_find.py
+
 #Class to represent a graph 
-class Graph: 
-  
+class Graph:
+
+
     def __init__(self,vertices): 
         self.V= vertices #No. of vertices 
         self.graph = [] # default dictionary  
-                                # to store graph 
-
+                                # to store graph
+        self.size = 0
 
     def addEdge(self,u,v,w): 
-        self.graph.append([u,v,w]) 
+        self.graph.append([u,v,w])
+        self.size += 1
 
 
     def KruskalMST(self):
-
+        mst = new Queue()
+        pq = new MinPQ,(graph.V())
+        uf = new UF(graph.V())
+        while pq and  self.size < graph.V()-1:
+            e = pq.delMin()
+            v = e.either(), w = e.other(v)
+            if(!connected()):
+                uf.union(v,w)
+                mst.enqueu(e)
     	return 1
--- lib/hw4/prims.py
+++ lib/hw4/prims.py
@@ -1,14 +1,14 @@
 import sys # Library for INT_MAX 
-  
-class Graph(): 
-  
-    def __init__(self, vertices): 
+
+class Graph():
+    Queue mst = new Queue()
+
+    def __init__(self, vertices):
         self.V = vertices 
         self.graph = [[0 for column in range(vertices)]  
                     for row in range(vertices)] 
 
     def primMST(self):
-        edgeTo = Edge[self().V]
-
+        Edge e = pq.delMin()
 
     	return 1

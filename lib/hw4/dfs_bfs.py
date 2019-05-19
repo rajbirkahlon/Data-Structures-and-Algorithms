dad59ea3a7d0d448782296f72de5e8787607b694
Raj <RajbirSingh1996@gmail.com>
2019-05-18 23:23:50 -0700

    addEdge fixed for dfs/bfs

--- lib/hw4/dfs_bfs.py
+++ lib/hw4/dfs_bfs.py
@@ -5,11 +5,20 @@ class Graph:
 		self.graph = defaultdict(list)
 		return
 
-	def addEdge(self,u,v): 
-    	self.graph[u].append(v)
+	def addEdge(self,u,v):
+		if v not in self.nodes:
+			self.add_node(v)
+			self.nodes[v].add_neighbor(self.nodes[u])
+		if u not in self.nodes:
+			self.add_node(u)
+			self.nodes[u].add_neighbor(self.nodes[v])
+		if u not in self.nodes and v not in self.nodes:
+			self.add_node(u)
+			self.add_node(v)
+			self.nodes[v].add_neighbor(self.nodes[u])
+			self.nodes[u].add_neighbor(self.nodes[v])
 
-
-    def dfs(self, v):
+def dfs(self, v):
 
 
 
